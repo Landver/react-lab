11f6b7a736279197b58fc47210b86265d3ac5613
Landver <good1ni@mail.ru>
2019-06-19 19:18:58 +0300

    update

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+import WebSocketButton from './WebSocketButton.jsx'
+import logo from '../logo.svg'
+import './App.css'
+
+function App () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+        <WebSocketButton />
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WebSocketButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class WebSocketButton extends React.Component {
+  render () {
+    return (
+      <div>
+        <button>WebSocketButton</button>
+      </div>
+    )
+  }
+}
+
+export default WebSocketButton
